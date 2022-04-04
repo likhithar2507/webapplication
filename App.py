8d771ddc7917cd5d760979402f791c447c3cd249
likhithar2507 <likhithagowda2507@gmail.com>
2022-04-04 10:11:36 +0530

    app

--- App.py
+++ App.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def welcome():
     return "WELCOME TO MY WEBSITE"
 
+@app.route('/contact')
+def contact():
+    return "Contact page"
+
+@app.route('/gallery')
+def gallery():
+    return " gallery "
+
+@app.route('/home')
+def home():
+    return "Home page"
+
 if __name__=="__main__":
     app.run()
